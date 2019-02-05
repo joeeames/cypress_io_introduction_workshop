91867a17e912e24e73cd8d54b8b376ca245117b8
Joe Eames <joeeames@gmail.com>
2019-02-05 14:05:56 -0700

    building more tests

--- cypress/integration/initial-page.spec.ts
+++ cypress/integration/initial-page.spec.ts
@@ -6,12 +6,14 @@
   describe('Initial page', () => {
 
     beforeEach(() => {
-      
+      cy.visit('dashboard');
+      cy.get('nav a').first().as('dashboardlink')
+      cy.get('nav a').last().as('heroeslink')
+
     })
 
     it(`has title 'Tour of Heroes'`, () => {
       // expect(browser.getTitle()).toEqual('Tour of Heroes');
-      cy.visit('http://localhost:4200/dashboard');
       cy.title()
         .should('eq', 'Tour of Heroes')
 
@@ -19,17 +21,66 @@
 
     it(`has h1 'Tour of Heroes'`, () => {
       // expectHeading(1, expectedH1);
-      cy.visit('http://localhost:4200/dashboard');
       cy.get('h1')
         .contains('Tour of Heroes')
         // be good to have them add this, and struggle a little bit, finding out that it uses rgb. give them a hint about using spaces
         .and('have.css', 'color', 'rgb(153, 153, 153)' )
     });
 
-    it(`has 4 heroes displaying`, () => {
-      cy.visit('http://localhost:4200/dashboard');
-      cy.get('app-dashboard a').should('have.length', 4);
+    it(`has the title of 'my heroes'`, () => {
+      cy.get('h3').contains('Top Heroes');
     })
+    
+    it(`has the title of 'Hero Search'`, () => {
+      cy.get('h4').contains('Hero Search');
+    })
+
+    it(`has a dashboard & heroes view link`, () => {
+      // method 1
+      // cy.get('nav a').first().contains('Dashboard').parent()
+      //   .get('a').last().contains('Heroes');
+
+      // method 2
+      // cy.get('nav a').as('views');
+      // cy.get('@views').first().contains('Dashboard');
+      // cy.get('@views').last().contains('Heroes');
+
+      // method 3
+      // put the following 2 into the beforeEach()
+      cy.get('@dashboardlink').contains('Dashboard');
+      cy.get('@heroeslink').contains('Heroes');
+
+    })
+
+
+    describe('heroes cards', () => {
+
+      it(`has 4 heroes displaying`, () => {
+        cy.get('app-dashboard a').should('have.length', 4);
+      })
+  
+      it(`has the module and hero classes`, () => {
+        cy.get('app-dashboard a div').should('have.class', 'module').and('have.class', 'hero')
+      })
+
+    });
+
+    describe.only('search box', () => {
+      it('displays matching results when text is input', () => {
+        cy.get('#search-box').type('Mr. Nice');
+        cy.get('.search-result li').contains('Mr. Nice').click();
+        cy.url().should('include', '/detail/11')
+      })
+    })
+
+    describe('messages', () => {
+      it('clears values when clear is clicked', () => {
+        cy.contains('clear').click();
+        cy.get('app-messages div div').should('have.length', 0);
+      })
+    })
+
+
 
     it('should fake out data', () => {
       cy.server();
@@ -41,7 +92,7 @@
           { "id": 43, "name": "Everything2" }
         ]
       })
-      cy.visit('http://localhost:4200/dashboard');
+      cy.visit('dashboard');
       cy.get('app-dashboard a').should('have.length', 1);
     })
 
