2b1be5b8b0dfa5e350cc0076862517b754cf731b
GregSCodEx <gregscodex@mixbox.pl>
2019-08-08 14:40:03 +0200

    Hero details: route

---
+++ cypress/integration/hero-details.spec.ts
@@ -0,0 +1,22 @@
+describe('Hero details', () => {
+  it(`should edit name`, () => {
+    cy.visit('detail/11');
+    
+    cy.server();
+    cy.route({
+      method: "PUT",
+      url: '/api/heroes/11',
+      response: {id: 11, name: 'Spider Pig'}
+    });
+    
+    cy.get('input').should('have.value', 'Mr. Nice');
+    
+    // cy.get('input').invoke('val').then((val) => {
+    //   expect(val).to.equal('Mr. Nice');
+    // })
+    
+    cy.get('input').clear().type('Spider Pig');
+    cy.get('h2').first().should('contain', 'SPIDER PIG');
+    // cy.contains('save').click();
+  })
+})
